5768bfc173fc833d26e965b09746425e38454d07
zhangsanhanchaofa‘ <2285460357@qq.com>
2021-10-13 19:15:49 +0800

    Initial commit

---
+++ src/main/java/com/kgc/Demo789Application.java
@@ -0,0 +1,13 @@
+package com.kgc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo789Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo789Application.class, args);
+    }
+
+}
